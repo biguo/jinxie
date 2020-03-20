e3345aecfb6dfc557e5899c7a7bab59479e2d140
Admin <admin@dev.com>
2020-03-20 08:48:47 +0800

    文章接口优化

--- app/Api/Controllers/ArticleController.php
+++ app/Api/Controllers/ArticleController.php
@@ -4,14 +4,19 @@ namespace App\Api\Controllers;
 
 
 use App\Models\Article;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 
 class ArticleController extends BaseController
 {
 
-    public function getList(Request $request)
+    public function getList()
     {
-        $data = Article::where(['status' => Status_Online, 'type_id' => $type = $request->input('type')])->get();
+        $params = Input::all();
+        $where = [['status', '=', Status_Online]];
+        isset($params['type']) ? array_push($where, ['type_id', '=', $params['type']]) : null;
+        $perPage = isset($params['perPage']) ? $params['perPage'] : 5;
+        $page = isset($params['page']) ? $params['page'] : 1;
+        $data = Article::where($where)->paginate($perPage, ['*'], 'page', $page);
         return responseSuccess($data);
     }
 
